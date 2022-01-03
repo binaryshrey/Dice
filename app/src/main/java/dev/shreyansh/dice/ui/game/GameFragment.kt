9b9989763b27825246b8fe8a4fd4f53f0fd40943
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2022-01-03 17:39:55 +0530

    adds overflow menu

--- app/src/main/java/dev/shreyansh/dice/ui/game/GameFragment.kt
+++ app/src/main/java/dev/shreyansh/dice/ui/game/GameFragment.kt
@@ -1,10 +1,9 @@
 package dev.shreyansh.dice.ui.game
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
@@ -26,7 +25,7 @@ class GameFragment : Fragment() {
         // Inflate the layout for this fragment
         (activity as AppCompatActivity).supportActionBar?.show()
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_game, container, false)
-
+        setHasOptionsMenu(true)
         Timber.i("GameFragment Inflated")
 
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
@@ -36,4 +35,18 @@ class GameFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.aboutFragment -> Toast.makeText(context,"About",Toast.LENGTH_SHORT).show()
+            R.id.rateUs -> Toast.makeText(context,"Rate",Toast.LENGTH_SHORT).show()
+            R.id.shareBugReport -> Toast.makeText(context,"Bug",Toast.LENGTH_SHORT).show()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
 }
