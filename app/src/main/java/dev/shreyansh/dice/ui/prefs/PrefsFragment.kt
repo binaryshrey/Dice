156c5dbdec89876a426e2335d631665cf156200f
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2022-11-14 13:25:20 +0530

    adds uncheck edge-case to prefs cardView

--- app/src/main/java/dev/shreyansh/dice/ui/prefs/PrefsFragment.kt
+++ app/src/main/java/dev/shreyansh/dice/ui/prefs/PrefsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import dev.shreyansh.dice.R
@@ -14,6 +15,7 @@ import dev.shreyansh.dice.databinding.FragmentPrefsBinding
 class PrefsFragment : Fragment() {
 
     private lateinit var binding: FragmentPrefsBinding
+    private var dicePref : String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,22 +27,28 @@ class PrefsFragment : Fragment() {
         binding.cardOne.setOnClickListener {
             uncheckCards()
             binding.cardOne.isChecked = true
+            dicePref = "one"
         }
         binding.cardTwo.setOnClickListener {
             uncheckCards()
             binding.cardTwo.isChecked = true
+            dicePref = "two"
         }
         binding.cardThree.setOnClickListener {
             uncheckCards()
             binding.cardThree.isChecked = true
+            dicePref = "three"
         }
         binding.cardFour.setOnClickListener {
             uncheckCards()
             binding.cardFour.isChecked = true
+            dicePref = "four"
         }
 
         binding.playButton.setOnClickListener {
-            view?.findNavController()?.navigate(R.id.action_prefsFragment_to_gameFragment)
+            if(dicePref.isEmpty())
+                Toast.makeText(context,"Select your board",Toast.LENGTH_SHORT).show()
+            else view?.findNavController()?.navigate(R.id.action_prefsFragment_to_gameFragment)
         }
         return binding.root
     }
