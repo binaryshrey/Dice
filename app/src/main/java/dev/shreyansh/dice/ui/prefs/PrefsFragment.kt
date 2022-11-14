bdeeda547e415aaafcde260ec7835a3e264cbf30
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2022-11-14 13:07:44 +0530

    adds dataBinding to prefs

--- app/src/main/java/dev/shreyansh/dice/ui/prefs/PrefsFragment.kt
+++ app/src/main/java/dev/shreyansh/dice/ui/prefs/PrefsFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import dev.shreyansh.dice.R
+import dev.shreyansh.dice.databinding.FragmentPrefsBinding
 
 
 class PrefsFragment : Fragment() {
+
+    private lateinit var binding: FragmentPrefsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_prefs, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_prefs, container, false)
+        return binding.root
     }
 
 }
